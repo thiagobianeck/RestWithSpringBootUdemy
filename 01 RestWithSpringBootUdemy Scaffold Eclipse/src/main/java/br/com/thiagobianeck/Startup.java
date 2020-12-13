b92f148ca94683c34533e47cc84ede257d615f5b
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-12-13 12:32:50 -0300

    Commit inicial

---
+++ 01 RestWithSpringBootUdemy Scaffold Eclipse/src/main/java/br/com/thiagobianeck/Startup.java
@@ -0,0 +1,14 @@
+package br.com.thiagobianeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Startup.class, args);
+	}
+
+
+}
